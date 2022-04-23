902ec322897145ed409adb8611a0920da7b0739c
an tran <an.uno94@gmail.com>
2022-04-23 15:32:34 +0700

    Add View Inventories

---
+++ IMS.Plugins.EFCore/InventoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterface;
+
+namespace IMS.Plugins.EFCore
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        public Task<IEnumerable<Inventory>> GetInventoriesByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
